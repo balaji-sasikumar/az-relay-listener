4a0b441ef590146a12a409e6f76931ee0a15f00f
balaji <balaji@theproindia.com>
2024-05-07 14:41:35 +0530

    fix:

--- listener.js
+++ listener.js
@@ -1,17 +1,11 @@
 const https = require("hyco-https");
 const axios = require("axios");
-
+const utils = require("util");
 const ns = process.env.ns;
 const path = process.env.path;
 const keyrule = process.env.keyrule;
 const key = process.env.key;
 
-console.log(
-  "namespace: " + ns,
-  "path: " + path,
-  "keyrule: " + keyrule,
-  "key: " + key
-);
 var uri = https.createRelayListenUri(ns, path);
 var server = https.createRelayedServer(
   {
@@ -19,19 +13,26 @@ var server = https.createRelayedServer(
     token: () => https.createRelayToken(uri, keyrule, key),
   },
   async (req, res) => {
-    const body = req.body;
-    console.log("request received: " + req.method + " on " + req.url, body);
-    const response = await axios.post(
-      "http://localhost:7071/api/queryTool",
-      body
-    );
-    console.log("request accepted: " + req.method + " on " + req.url);
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(response.data));
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      console.log(body);
+      const response = await axios.post(
+        "http://localhost:7071/api/queryTool",
+        body
+      );
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(response.data));
+    });
   }
 );
 server.listen();
 
 server.on("error", (err) => {
-  console.log("error: " + JSON.stringify(err));
+  console.log(utils.inspect(err));
+});
+server.on("listening", () => {
+  console.log("listening");
 });
